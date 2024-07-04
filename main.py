6eec8c8b038ea50fa097662ab39accbf23c179e7
Marxu <fuhaojun@52tt.com>
2024-07-04 20:48:11 +0800

    feat: 完成 酒店 available 和 book 功能

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import pandas as pd
 # class User:
 #     pass
 
-df = pd.read_csv('hotels.csv')
+df = pd.read_csv('hotels.csv', dtype=str)
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        df.loc[df['id'] == self.hotel_id ,'available'] = 'no'
+        df.to_csv('hotels.csv', index=False)
 
     def available(self):
-        pass
+        availability = df.loc[df['id'] == self.hotel_id, 'available'].squeeze()
+        if availability == 'yes':
+            return True
+        else:
+            return False
 
 class ReservationTicket:
     def __init__(self,customer_name, customer_object):
@@ -24,8 +29,8 @@ class ReservationTicket:
 
 
 # print(df) 拿到整个表
-id = input('Enter the id of the hotel: ')
-hotel = Hotel(id)
+hotel_id = input('Enter the id of the hotel: ')
+hotel = Hotel(hotel_id)
 if hotel.available():
     hotel.book()
     name = input('Enter your name: ')
