3fde0974b851ea66a9d99f9b7dbb7b8c35c411ab
Marxu <fuhaojun@52tt.com>
2024-07-04 21:05:47 +0800

    feat: 补充 买票的 generate ，注意 ReservationTicket 如何引用的 hotel 里的 name

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ df = pd.read_csv('hotels.csv', dtype=str)
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df['id'] == self.hotel_id, 'name'].squeeze()
 
     def book(self):
         df.loc[df['id'] == self.hotel_id ,'available'] = 'no'
@@ -21,18 +22,30 @@ class Hotel:
             return False
 
 class ReservationTicket:
-    def __init__(self,customer_name, customer_object):
-        pass
+    def __init__(self,customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name:{self.customer_name}
+        Hotel name:{self.hotel.name}
+        """
+
+        return content
 
 
 # print(df) 拿到整个表
 hotel_id = input('Enter the id of the hotel: ')
 hotel = Hotel(hotel_id)
+
 if hotel.available():
     hotel.book()
     name = input('Enter your name: ')
-    reservation_ticket = ReservationTicket(name, hotel)
-    reservation_ticket.generate()
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+    res = reservation_ticket.generate()
+    print(res)
+else:
+    print('Hotel is not free!')
