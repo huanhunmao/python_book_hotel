2f3d3110221c73d86657a68f0d955debfba16f12
Marxu <fuhaojun@52tt.com>
2024-07-05 11:33:38 +0800

    feat: 添加一个 信用卡 💳验证，通过才能预定

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 #     pass
 
 df = pd.read_csv('hotels.csv', dtype=str)
+df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
+print(df_cards)
+
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -21,6 +24,7 @@ class Hotel:
         else:
             return False
 
+
 class ReservationTicket:
     def __init__(self,customer_name, hotel_object):
         self.customer_name = customer_name
@@ -37,15 +41,37 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {
+            'number': self.number,
+            'expiration': expiration,
+            'holder': holder,
+            'cvc': cvc
+        }
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 # print(df) 拿到整个表
 hotel_id = input('Enter the id of the hotel: ')
 hotel = Hotel(hotel_id)
 
 if hotel.available():
-    hotel.book()
-    name = input('Enter your name: ')
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    res = reservation_ticket.generate()
-    print(res)
+    card_data = CreditCard(number='5678')
+    if card_data.validate(expiration='12/28',
+                           holder='JANE SMITH', cvc='456'):
+        hotel.book()
+        name = input('Enter your name: ')
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        res = reservation_ticket.generate()
+        print(res)
+    else:
+        print('Please check your payment!')
 else:
     print('Hotel is not free!')
